8bcddd7a6a5b876c16ad0e832bf1ba07e8c00844
Anton Solback <solback@student.chalmers.se>
2019-04-11 17:22:43 +0200

    Made some changes to the history view
    However, I would not consider this view stable and is at the moment not enabled due to it not being stable.

--- bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/ui/views/FeatureListView.java
+++ bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/ui/views/FeatureListView.java
@@ -6,9 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
-import org.eclipse.jface.viewers.CheckboxTableViewer;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.IColorProvider;
@@ -23,7 +21,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
@@ -31,7 +28,6 @@ import se.gu.featuredashboard.model.featuremodel.Feature;
 import se.gu.featuredashboard.model.featuremodel.FeatureContainer;
 import se.gu.featuredashboard.model.featuremodel.Project;
 import se.gu.featuredashboard.model.featuremodel.ProjectStore;
-import se.gu.featuredashboard.ui.providers.FeatureTableLabelProvider;
 import se.gu.featuredashboard.utils.FeaturedashboardConstants;
 import se.gu.featuredashboard.utils.IUpdateInformationListener;
 import se.gu.featuredashboard.utils.MetricsComparator;
@@ -39,88 +35,93 @@ import se.gu.featuredashboard.utils.ParseProjectAction;
 import se.gu.featuredashboard.utils.SelectionHandler;
 
 public class FeatureListView extends ViewPart implements IUpdateInformationListener {
-	
+
 	private CheckboxTreeViewer treeViewer;
 	private IWorkbenchWindow window;
-	
+
 	private ParseProjectAction parseProject;
 	private Action sortTableAction;
-	
+
 	private Project activeProject;
-	
+
 	@Override
 	public void createPartControl(Composite parent) {
 		window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		
+
 		treeViewer = new CheckboxTreeViewer(parent);
 		treeViewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
 		treeViewer.setContentProvider(new FeatureTreeContentProvider());
 		treeViewer.setLabelProvider(new FeatureTreeLabelProvider());
-		
+
 		treeViewer.addCheckStateListener(new ICheckStateListener() {
 
 			@Override
 			public void checkStateChanged(CheckStateChangedEvent event) {
 				Display.getDefault().asyncExec(() -> {
-					
+
 					treeViewer.setSubtreeChecked(event.getElement(), event.getChecked());
-					
+
 					List<FeatureContainer> featureFileList = new ArrayList<>();
 
-					if(treeViewer.getCheckedElements().length > 0) {				
+					if (treeViewer.getCheckedElements().length > 0) {
 						Arrays.stream(treeViewer.getCheckedElements()).forEach(checkedElement -> {
 							FeatureContainer feature = (FeatureContainer) checkedElement;
 							featureFileList.add(feature);
 						});
 					}
-					
+
 					SelectionHandler.setSelection(featureFileList);
-					
-					FeatureFileView featureFileView = (FeatureFileView) window.getActivePage().findView(FeaturedashboardConstants.FEATUREFILE_VIEW_ID);
-					if(featureFileView != null)
+
+					FeatureFileView featureFileView = (FeatureFileView) window.getActivePage()
+							.findView(FeaturedashboardConstants.FEATUREFILE_VIEW_ID);
+					if (featureFileView != null)
 						featureFileView.inputToView(featureFileList);
 
-					FeatureFolderView featureFolderView = (FeatureFolderView) window.getActivePage().findView(FeaturedashboardConstants.FEATUREFOLDER_VIEW_ID);
-					if(featureFolderView != null)
+					FeatureFolderView featureFolderView = (FeatureFolderView) window.getActivePage()
+							.findView(FeaturedashboardConstants.FEATUREFOLDER_VIEW_ID);
+					if (featureFolderView != null)
 						featureFolderView.inputToView(featureFileList);
 
 					// Work in progress
-//					HistoryView historyView = (HistoryView) window.getActivePage().findView(FeaturedashboardConstants.HISTORY_VIEW_ID);
-//					if(historyView != null)
+//					HistoryView historyView = (HistoryView) window.getActivePage()
+//							.findView(FeaturedashboardConstants.HISTORY_VIEW_ID);
+//					if (historyView != null)
 //						historyView.inputToView(featureFileList);
 				});
 			}
-			
+
 		});
-		
+
 		MetricsComparator comparator = new MetricsComparator();
 		treeViewer.setComparator(comparator);
-		
+
 		parseProject = new ParseProjectAction(parent.getShell());
 		parseProject.setText(FeaturedashboardConstants.ACTION_TEXT);
 		parseProject.setToolTipText(FeaturedashboardConstants.ACTION_TOOLTOP_TEXT);
-		parseProject.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_REDO));
+		parseProject.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_REDO));
 		addActionToView(parseProject);
-		
+
 		sortTableAction = new Action() {
 			public void run() {
 				comparator.setColumn(FeaturedashboardConstants.FEATURELISTTABLE_SORT_COLUMN);
 				treeViewer.refresh();
 			}
 		};
-		
+
 		sortTableAction.setText("Sort alphabetical order");
 		sortTableAction.setToolTipText("Sort table");
-		sortTableAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_UP));
+		sortTableAction.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_UP));
 		addActionToView(sortTableAction);
-		
+
 		updateData();
 	}
-	
+
 	private void addActionToView(Action action) {
 		getViewSite().getActionBars().getMenuManager().add(action);
 	}
-	
+
 	@Override
 	public void setFocus() {
 	}
@@ -129,31 +130,31 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 	public void updateData() {
 		Display.getDefault().asyncExec(() -> {
 			activeProject = ProjectStore.getActiveProject();
-			if(activeProject != null) {
+			if (activeProject != null) {
 				treeViewer.setInput(activeProject);
 				treeViewer.refresh();
 			}
 		});
 	}
-	
+
 	public class FeatureTreeContentProvider implements ITreeContentProvider {
-		
+
 		@Override
 		public Object[] getElements(Object inputElement) {
 			activeProject = (Project) inputElement;
-			
+
 			List<FeatureContainer> list = new ArrayList<>();
-			
-			for(Feature feature : activeProject.getRootFeatures()) {
+
+			for (Feature feature : activeProject.getRootFeatures()) {
 				FeatureContainer fc = activeProject.getFeatureContainer(feature);
-				if(fc != null)
+				if (fc != null)
 					list.add(fc);
 			}
-			
+
 			list.addAll(activeProject.getFeatureContainers().stream().filter(featureContainer -> {
 				return !activeProject.getFeaturesInModel().contains(featureContainer.getFeature());
-			}).collect(Collectors.toList()));		
-			
+			}).collect(Collectors.toList()));
+
 			return list.toArray();
 		}
 
@@ -161,24 +162,24 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 		public Object[] getChildren(Object parentElement) {
 			FeatureContainer parent = (FeatureContainer) parentElement;
 			List<FeatureContainer> children = new ArrayList<>();
-			for(Feature feature : parent.getFeature().getSubFeatures()) {
+			for (Feature feature : parent.getFeature().getSubFeatures()) {
 				FeatureContainer fc = activeProject.getFeatureContainer(feature);
-				if(fc != null)
+				if (fc != null)
 					children.add(activeProject.getFeatureContainer(feature));
 			}
-			
+
 			return children.toArray();
 		}
 
 		@Override
 		public Object getParent(Object element) {
-			FeatureContainer child = (FeatureContainer) element; 
-			for(Feature feature : activeProject.getRootFeatures()) {
-				if(feature.getSubFeatures().contains(child.getFeature())) {
+			FeatureContainer child = (FeatureContainer) element;
+			for (Feature feature : activeProject.getRootFeatures()) {
+				if (feature.getSubFeatures().contains(child.getFeature())) {
 					return activeProject.getFeatureContainer(feature);
-				}		
+				}
 			}
-			
+
 			return null;
 		}
 
@@ -189,19 +190,19 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 		}
 
 	}
-	
+
 	public class FeatureTreeLabelProvider implements ILabelProvider, IColorProvider {
 
 		@Override
 		public void addListener(ILabelProviderListener listener) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void dispose() {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
@@ -213,7 +214,7 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 		@Override
 		public void removeListener(ILabelProviderListener listener) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
@@ -231,7 +232,7 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 		@Override
 		public Color getForeground(Object element) {
 			FeatureContainer container = (FeatureContainer) element;
-			if(activeProject.getFeaturesInModel().contains(container.getFeature())) {
+			if (activeProject.getFeaturesInModel().contains(container.getFeature())) {
 				return Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
 			}
 			return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
@@ -242,7 +243,6 @@ public class FeatureListView extends ViewPart implements IUpdateInformationListe
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
 	}
 }
-
--- bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/ui/views/HistoryView.java
+++ bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/ui/views/HistoryView.java
@@ -39,7 +39,7 @@ import se.gu.featuredashboard.utils.ParseJob;
 public class HistoryView extends ViewPart {
 
 	private static final int STEPS_IN_HISTORY = 4;
-	
+
 	private static final String GIT_ERROR_FATAL = "fatal: not a git repository (or any of the parent directories): .git";
 	private static final String GIT_COMMIT_HASH = "git log -n " + STEPS_IN_HISTORY + " --pretty=format:\"%H\"";
 	private static final String GIT_STASH = "git stash";
@@ -47,55 +47,49 @@ public class HistoryView extends ViewPart {
 	private static final String GIT_BRANCH = "git rev-parse --abbrev-ref HEAD";
 	private static final String GIT_STASH_POP = "git stash pop";
 	private static final String GIT_DIFF = "git diff --name-status ";
-	
+
 	private static final String GIT_ADDED = "A";
 	private static final String GIT_DELETED = "D";
 	private static final String GIT_MODIFIED = "M";
-	
+
 	private static final String CMD_EXEC = "cmd.exe /c ";
 	private static final String APPEND_COMMAND = " && ";
-	
+
 	private static final String X_AXIS_LABEL = "Commits ago";
 	private static final String Y_AXIS_LABEL = "Value";
-	
+
 	private Axis xAxis;
 	private Axis yAxis;
 	private IXYGraph graph;
 	private LightweightSystem lws;
-	
+
 	private Map<String, Tuple<Trace, double[]>> allTraces = new HashMap<>();
 	private Map<String, Tuple<Trace, double[]>> activeTraces = new HashMap<>();
-	
-	private static final List<String> METRICS = Arrays.asList(
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_2_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_3_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_4_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_5_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_6_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_7_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_8_NAME,
-			FeaturedashboardConstants.FEATURETABLE_COLUMN_9_NAME
-			);
-	
+
+	private static final List<String> METRICS = Arrays.asList(FeaturedashboardConstants.FEATURETABLE_COLUMN_2_NAME,
+			FeaturedashboardConstants.FEATURETABLE_COLUMN_3_NAME, FeaturedashboardConstants.FEATURETABLE_COLUMN_4_NAME,
+			FeaturedashboardConstants.FEATURETABLE_COLUMN_5_NAME, FeaturedashboardConstants.FEATURETABLE_COLUMN_6_NAME,
+			FeaturedashboardConstants.FEATURETABLE_COLUMN_7_NAME, FeaturedashboardConstants.FEATURETABLE_COLUMN_8_NAME,
+			FeaturedashboardConstants.FEATURETABLE_COLUMN_9_NAME);
+
 	@Override
 	public void createPartControl(Composite parent) {
 		Canvas graphCanvas = new Canvas(parent, SWT.BORDER);
-		
+
 		lws = new LightweightSystem(graphCanvas);
 		graph = new XYGraph();
-		
-		
+
 		lws.setContents(graph);
-		
+
 		xAxis = graph.getPrimaryXAxis();
 		xAxis.setTitle(X_AXIS_LABEL);
 		xAxis.setFormatPattern("#.#");
 		xAxis.setRange(STEPS_IN_HISTORY, 1);
-		
+
 		yAxis = graph.getPrimaryYAxis();
 		yAxis.setTitle(Y_AXIS_LABEL);
 		yAxis.setFormatPattern("#");
-		
+
 		parent.addMouseWheelListener(new MouseWheelListener() {
 			@Override
 			public void mouseScrolled(org.eclipse.swt.events.MouseEvent e) {
@@ -103,11 +97,11 @@ public class HistoryView extends ViewPart {
 				yAxis.zoomInOut(valuePosition, e.count * 0.1 / 3);
 			}
 		});
-		
+
 		METRICS.forEach(metric -> {
 			Action action = new Action() {
 				public void run() {
-					if(activeTraces.containsKey(metric))
+					if (activeTraces.containsKey(metric))
 						graph.removeTrace(activeTraces.remove(metric).getLeft());
 					else {
 						Tuple<Trace, double[]> traceInfo = allTraces.get(metric);
@@ -123,25 +117,26 @@ public class HistoryView extends ViewPart {
 	}
 
 	@Override
-	public void setFocus() {} 
+	public void setFocus() {
+	}
 
 	public void inputToView(List<FeatureContainer> featureContainers) {
-		if(featureContainers.size() == 0)
+		if (featureContainers.size() == 0)
 			return;
-		
+
 		activeTraces.values().stream().map(Tuple::getLeft).forEach(graph::removeTrace);
-		
+
 		activeTraces = new HashMap<>();
 		allTraces = new HashMap<>();
-		
+
 		String projectLocation = ProjectStore.getActiveProject().getLocation().toString();
 		String startingBranch = null;
-		
+
 		StringBuilder commandToExecute;
 		Process p;
-		
+
 		List<Triple<String, Project, List<String>>> diffInfo = new ArrayList<>();
-		
+
 		try {
 			commandToExecute = new StringBuilder();
 			commandToExecute.append(CMD_EXEC);
@@ -149,99 +144,103 @@ public class HistoryView extends ViewPart {
 			commandToExecute.append(APPEND_COMMAND);
 			commandToExecute.append(GIT_STASH);
 			p = Runtime.getRuntime().exec(commandToExecute.toString());
-			
-			try(BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getErrorStream()))){
+
+			try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
 				String line = null;
-				while((line = stdInput.readLine()) != null) {
+				while ((line = stdInput.readLine()) != null) {
 					// Not a Git directory, return
-					if(line.equals(GIT_ERROR_FATAL))
+					if (line.equals(GIT_ERROR_FATAL))
 						return;
-				}					
+				}
 			}
-			
+
 			commandToExecute = new StringBuilder();
 			commandToExecute.append(CMD_EXEC);
 			commandToExecute.append("cd " + projectLocation);
 			commandToExecute.append(APPEND_COMMAND);
 			commandToExecute.append(GIT_BRANCH);
 			p = Runtime.getRuntime().exec(commandToExecute.toString());
-			
-			try(BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()))){
+
+			try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
 				String line = null;
-				while((line = stdInput.readLine()) != null) {
+				while ((line = stdInput.readLine()) != null) {
 					startingBranch = line;
-				}					
+				}
 			}
-			
+
 			commandToExecute = new StringBuilder();
 			commandToExecute.append(CMD_EXEC);
 			commandToExecute.append("cd " + projectLocation);
 			commandToExecute.append(APPEND_COMMAND);
 			commandToExecute.append(GIT_COMMIT_HASH);
 			p = Runtime.getRuntime().exec(commandToExecute.toString());
-			
-			try(BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()))){
+
+			try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
 				String line = null;
-				while((line = stdInput.readLine()) != null)
-					diffInfo.add(new Triple<String, Project, List<String>>(line, ProjectStore.getActiveProject().clone(), new ArrayList<>()));
+				while ((line = stdInput.readLine()) != null) {
+					diffInfo.add(new Triple<String, Project, List<String>>(line,
+							ProjectStore.getActiveProject().clone(), new ArrayList<>()));
+				}
 			}
-			
-			for(Triple<String, Project, List<String>> entry : diffInfo) {		
+
+			for (Triple<String, Project, List<String>> entry : diffInfo) {
 				List<String> diffs = entry.getRight();
 				Project project = entry.getCenter();
-				
+
 				commandToExecute = new StringBuilder();
 				commandToExecute.append(CMD_EXEC);
 				commandToExecute.append("cd " + projectLocation);
 				commandToExecute.append(APPEND_COMMAND);
 				commandToExecute.append(GIT_DIFF + entry.getLeft());
 				p = Runtime.getRuntime().exec(commandToExecute.toString());
-				
-				try(BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))){
+
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
 					String line = null;
-					while((line = br.readLine()) != null) {
+					while ((line = br.readLine()) != null) {
 						diffs.add(line);
 					}
 				}
-				
-				// After diffs have been obtained, checkout commit and do appropriate action 
+
+				// After diffs have been obtained, checkout commit and do appropriate action
 				commandToExecute = new StringBuilder();
 				commandToExecute.append(CMD_EXEC);
 				commandToExecute.append("cd " + projectLocation);
 				commandToExecute.append(APPEND_COMMAND);
 				commandToExecute.append(GIT_CHECKOUT + entry.getLeft());
 				p = Runtime.getRuntime().exec(commandToExecute.toString());
-				
-				for(String diff : diffs) {
+
+				for (String diff : diffs) {
 					String[] info = diff.split("\\s+");
-					
-					IFile file = project.getIProject().getWorkspace().getRoot().getFile(new Path("/" + project.getID() + "/" + info[1]));
-					
-					System.out.println("Fileeee: " + file.getLocation().toString());
-					
-					if(info[0].equals(GIT_ADDED)) {
-						// If the diff show that the file has been added then it needs to be removed from this project
+
+					IFile file = project.getIProject().getWorkspace().getRoot()
+							.getFile(new Path("/" + project.getID() + "/" + info[1]));
+
+					if (info[0].equals(GIT_ADDED)) {
+						// If the diff show that the file has been added then it needs to be removed
+						// from this project
 						project.getFeatureContainers().stream()
-													  .filter(featureContainer -> featureContainer.isMappedIn(file) || featureContainer.isAnnotatedIn(file))
-													  .forEach(featureContainer -> featureContainer.removeFile(file));
-						
-					} else if(info[0].equals(GIT_DELETED) || info[0].equals(GIT_MODIFIED)) {
-						// If the diff showd that the file was removed/modified then we need to reparse it.
-						ParseJob job = new ParseJob("Parse history", project, file, Display.getDefault().getActiveShell());
+								.filter(featureContainer -> featureContainer.isMappedIn(file)
+										|| featureContainer.isAnnotatedIn(file))
+								.forEach(featureContainer -> featureContainer.removeFile(file));
+
+					} else if (info[0].equals(GIT_DELETED) || info[0].equals(GIT_MODIFIED)) {
+						// If the diff showd that the file was removed/modified then we need to reparse
+						// it.
+						ParseJob job = new ParseJob("Parse history", project, file,
+								Display.getDefault().getActiveShell());
 						job.setUser(true);
 						job.schedule();
 					}
 				}
-				
+
 				commandToExecute = new StringBuilder();
 				commandToExecute.append(CMD_EXEC);
 				commandToExecute.append("cd " + projectLocation);
 				commandToExecute.append(APPEND_COMMAND);
-				commandToExecute.append(GIT_CHECKOUT + startingBranch);
-				System.out.println("Command " + commandToExecute.toString());
+				commandToExecute.append(GIT_CHECKOUT + startingBranch + " > nul 2>&1");
 				p = Runtime.getRuntime().exec(commandToExecute.toString());
 			}
-		} catch(IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			// Checkout correct branch and pop stash
@@ -251,60 +250,63 @@ public class HistoryView extends ViewPart {
 			commandToExecute.append(APPEND_COMMAND);
 			commandToExecute.append(GIT_CHECKOUT + startingBranch);
 			commandToExecute.append(APPEND_COMMAND);
-			commandToExecute.append(GIT_STASH_POP);
-			System.out.println("Command " + commandToExecute.toString());
+			commandToExecute.append(GIT_STASH_POP + " > nul 2>&1");
 			try {
 				p = Runtime.getRuntime().exec(commandToExecute.toString());
-			} catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		IntStream.range(0, diffInfo.size()).forEach(i -> {	
-			FeatureContainer featureContainer = diffInfo.get(i).getCenter().getFeatureContainer(featureContainers.get(0).getFeature());
-			
+
+		IntStream.range(0, diffInfo.size()).forEach(i -> {
+			FeatureContainer featureContainer = diffInfo.get(i).getCenter()
+					.getFeatureContainer(featureContainers.get(0).getFeature());
+
 			METRICS.forEach(metric -> {
 				Tuple<Trace, double[]> traceInfo = allTraces.get(metric);
-				if(traceInfo == null)
-					traceInfo = new Tuple<Trace, double[]>(new Trace(metric, xAxis, yAxis, new CircularBufferDataProvider(true)), new double[STEPS_IN_HISTORY+1]);
-					
-				switch(metric) {
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_2_NAME:
-						traceInfo.getRight()[i] = featureContainer.getLOFC();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_3_NAME:
-						traceInfo.getRight()[i] = featureContainer.getNumberOfFileAnnotations();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_4_NAME:
-						traceInfo.getRight()[i] = featureContainer.getNumberOfFolderAnnotations();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_5_NAME:
-						traceInfo.getRight()[i] = featureContainer.getTanglingDegree();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_6_NAME:
-						traceInfo.getRight()[i] = featureContainer.getScatteringDegree();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_7_NAME:
-						traceInfo.getRight()[i] = featureContainer.getMaxND();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_8_NAME:
-						traceInfo.getRight()[i] = featureContainer.getAvgND();
-						break;
-					case FeaturedashboardConstants.FEATURETABLE_COLUMN_9_NAME:
-						traceInfo.getRight()[i] = featureContainer.getMinND();
-						break;
+				if (traceInfo == null)
+					traceInfo = new Tuple<Trace, double[]>(
+							new Trace(metric, xAxis, yAxis, new CircularBufferDataProvider(true)),
+							new double[STEPS_IN_HISTORY + 1]);
+
+				switch (metric) {
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_2_NAME:
+					traceInfo.getRight()[i] = featureContainer.getLOFC();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_3_NAME:
+					traceInfo.getRight()[i] = featureContainer.getNumberOfFileAnnotations();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_4_NAME:
+					traceInfo.getRight()[i] = featureContainer.getNumberOfFolderAnnotations();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_5_NAME:
+					traceInfo.getRight()[i] = featureContainer.getTanglingDegree();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_6_NAME:
+					traceInfo.getRight()[i] = featureContainer.getScatteringDegree();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_7_NAME:
+					traceInfo.getRight()[i] = featureContainer.getMaxND();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_8_NAME:
+					traceInfo.getRight()[i] = featureContainer.getAvgND();
+					break;
+				case FeaturedashboardConstants.FEATURETABLE_COLUMN_9_NAME:
+					traceInfo.getRight()[i] = featureContainer.getMinND();
+					break;
 				}
 				allTraces.put(metric, traceInfo);
 			});
-			
+
 		});
-		
+
 		allTraces.entrySet().stream().forEach(entry -> {
 			Trace trace = entry.getValue().getLeft();
 			trace.setPointStyle(PointStyle.CIRCLE);
 			CircularBufferDataProvider traceDataProvider = (CircularBufferDataProvider) trace.getDataProvider();
-			// When all data has been collected set the data points with the correct data provider
+			// When all data has been collected set the data points with the correct data
+			// provider
 			traceDataProvider.setCurrentYDataArray(entry.getValue().getRight());
-		});		
+		});
 	}
 }
