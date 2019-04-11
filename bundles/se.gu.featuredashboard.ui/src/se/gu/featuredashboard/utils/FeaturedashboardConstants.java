f62efa2d05fa3a0ecdce9edfeb9ce96b1769a1be
Anton Solback <solback@student.chalmers.se>
2019-04-11 20:18:15 +0200

    Added preference to parser

--- bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/utils/FeaturedashboardConstants.java
+++ bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/utils/FeaturedashboardConstants.java
@@ -7,7 +7,7 @@ public class FeaturedashboardConstants {
 	// Parse action in FeatureListView
 	public static final String ACTION_TEXT = "Parse selected project";
 	public static final String ACTION_TOOLTOP_TEXT = "Parses the selected project in the Project/Package Exlorer";
-	
+
 	// ID's
 	public static final String HISTORY_VIEW_ID = "se.gu.featuredashboard.ui.views.HistoryView";
 	public static final String FEATUREFOLDER_VIEW_ID = "se.gu.featuredashboard.ui.views.FeatureFolderView";
@@ -20,85 +20,87 @@ public class FeaturedashboardConstants {
 	public static final String BUILDER_ID = "se.gu.featuredashboard.utils.Builder";
 	public static final String FEATURE_MARKER_ID = "se.gu.featuredashboard.ui.AnnotationMarker";
 	public static final String REMOVEMARKERS_COMMAND_ID = "se.gu.featuredashboard.ui.commands.RemoveMarkers";
-	
+
 	public static final String FEATUREDASHBOARD_FOLDER_PATH = ".featuredashboard";
-	
+
+	public static final String FEATUREDASHBOARD_PREFERENCES = "se.gu.featuredashboard.ui.mainPreferences.page";
+
 	public static final Dimension NODE_SIZE = new Dimension(75, 25);
-	public static final Dimension NODE_SPACING = new Dimension(NODE_SIZE.width+30, NODE_SIZE.height+30);
-	
+	public static final Dimension NODE_SPACING = new Dimension(NODE_SIZE.width + 30, NODE_SIZE.height + 30);
+
 	public static final String GRAPH_ID_KEY = "GRAPH_ID";
 	public static final String NESTEDGRAPH_ID = "GRAPH_ID";
-	
+
 	// File name associated strings
 	public static final String FEATUREFILE_FILE = "feature-file";
 	public static final String FEATUREFOLDER_FILE = "feature-folder";
 	public static final String VPFILE_FILE = "vp-files";
 	public static final String VPFOLDER_FILE = "vp-folder";
-	
+
 	public static final String EXPORT_FILENAME = "FeatureMetrics.csv";
-	
+
 	public static final String SYNTAXERROR_DIALOG_TITLE = "Parsing Errors!";
 	public static final String SYNTAXERROR_DIALOG_MESSAGE = "Errors occured when parsing resources";
-	
+
 	// Column names/tooltips for FeatureMetricsView
 	public static final String FEATURETABLE_COLUMN_1_NAME = "Feature";
 	public static final String FEATURETABLE_COLUMN_1_TOOLTIP = "The feature of interest";
 	public static final int FEATURETABLE_COLUMN_1_SIZE = 175;
-	
+
 	public static final String FEATURETABLE_COLUMN_2_NAME = "LOFC";
 	public static final String FEATURETABLE_COLUMN_2_TOOLTIP = "How many annotated lines for this feature";
-	
+
 	public static final String FEATURETABLE_COLUMN_3_NAME = "NoFiA";
 	public static final String FEATURETABLE_COLUMN_3_TOOLTIP = "How many in-file annotations for this feature";
-	
+
 	public static final String FEATURETABLE_COLUMN_4_NAME = "NoFoA";
 	public static final String FEATURETABLE_COLUMN_4_TOOLTIP = "How folders directly referensing this this feature";
-	
+
 	public static final String FEATURETABLE_COLUMN_5_NAME = "Tangling Degree";
 	public static final String FEATURETABLE_COLUMN_5_TOOLTIP = "How many other features share the same artefact as this feature";
-	
+
 	public static final String FEATURETABLE_COLUMN_6_NAME = "Scattering Degree";
 	public static final String FEATURETABLE_COLUMN_6_TOOLTIP = "Across how many artefacts is this feature implemented in";
-	
+
 	public static final String FEATURETABLE_COLUMN_7_NAME = "Max Nesting degree";
 	public static final String FEATURETABLE_COLUMN_7_TOOLTIP = "Max nesting (folder) degree of annotations";
-	
+
 	public static final String FEATURETABLE_COLUMN_8_NAME = "Avg Nesting degree";
 	public static final String FEATURETABLE_COLUMN_8_TOOLTIP = "Avg esting (folder) degree of annotations";
-	
+
 	public static final String FEATURETABLE_COLUMN_9_NAME = "Min Nesting degree";
 	public static final String FEATURETABLE_COLUMN_9_TOOLTIP = "Min esting (folder) degree of annotations";
-	
+
 	public static final String FEATURETABLE_ID = "FEATURE_TABLE";
-	
-	// Column names/tooltips for ProjectMetricsView	
+
+	// Column names/tooltips for ProjectMetricsView
 	public static final String PROJECTTABLE_COLUMN_1_NAME = "Project";
 	public static final String PROJECTTABLE_COLUMN_1_TOOLTIP = "Current selected project";
-	
+
 	public static final String PROJECTTABLE_COLUMN_2_NAME = "Features";
 	public static final String PROJECTTABLE_COLUMN_2_TOOLTIP = "How many distict features does this project contain";
-	
+
 	public static final String PROJECTTABLE_COLUMN_3_NAME = "Total LoFC";
 	public static final String PROJECTTABLE_COLUMN_3_TOOLTIP = "Total lines of feature code";
-	
+
 	public static final String PROJECTTABLE_COLUMN_4_NAME = "Avg LoFC";
 	public static final String PROJECTTABLE_COLUMN_4_TOOLTIP = "Average lines of feature code";
-	
+
 	public static final String PROJECTTABLE_COLUMN_5_NAME = "Avg Nesting Degree";
 	public static final String PROJECTTABLE_COLUMN_5_TOOLTIP = "Average nesting degree";
-	
+
 	public static final String PROJECTTABLE_COLUMN_6_NAME = "Avg Scattering Degree";
 	public static final String PROJECTTABLE_COLUMN_6_TOOLTIP = "Average scattering degree";
-	
+
 	public static final int FEATURELISTTABLE_SORT_COLUMN = 99;
 	public static final int DOUBLECLICK_DURATION = 500;
-	
+
 	public static final String PROJECTTABLE_ID = "PROJECT_TABLE";
 
 	// When selecting something that isn't a project in package/project explorer
 	public static final String WRONG_SELECTION_MESSAGE = "Current selection is not a project";
-	
+
 	// Used to determine which operation is responsible for graph node clicks
 	public static final String NODE_ONCLICK_OPERATION_ID = "OnClickOperation";
-	
+
 }
--- bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/utils/ParseJob.java
+++ bundles/se.gu.featuredashboard.ui/src/se/gu/featuredashboard/utils/ParseJob.java
@@ -20,11 +20,13 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+import org.osgi.service.prefs.Preferences;
 
 import se.gu.featuredashboard.model.featuremodel.Feature;
 import se.gu.featuredashboard.model.featuremodel.FeatureContainer;
@@ -46,24 +48,26 @@ public class ParseJob extends Job {
 	private IFile fileToParse;
 	private Shell shell;
 	private Map<Feature, FeatureContainer> projectFeatures;
+	private List<String> excludedFolders;
 	private Logger logger = PlatformUI.getWorkbench().getService(org.eclipse.e4.core.services.log.Logger.class);
 	private JobType jobType;
 	private ErrorDialog errorDialog;
 	private List<Triple<String, String, Integer>> parsingExceptions;
-	
+
 	private enum JobType {
 		FULL, SINGLE
 	};
-	
+
 	public ParseJob(String name, Project project, Shell shell) {
 		super(name);
 		this.project = project;
 		this.shell = shell;
 		projectFeatures = new HashMap<>();
 		parsingExceptions = new ArrayList<>();
+		excludedFolders = new ArrayList<>();
 		jobType = JobType.FULL;
 	}
-	
+
 	public ParseJob(String name, Project project, IFile file, Shell shell) {
 		super(name);
 		this.project = project;
@@ -71,287 +75,316 @@ public class ParseJob extends Job {
 		this.shell = shell;
 		projectFeatures = new HashMap<>();
 		parsingExceptions = new ArrayList<>();
+		excludedFolders = new ArrayList<>();
 		jobType = JobType.SINGLE;
 	}
 
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
+		Preferences preferences = ConfigurationScope.INSTANCE
+				.getNode(FeaturedashboardConstants.FEATUREDASHBOARD_PREFERENCES);
+		Preferences sub1 = preferences.node("node1");
+
+		if (sub1.get("initialized", "default").equals("yes")) {
+			int i = 0;
+			while (true) {
+				String folderKey = "Output" + i;
+				String folderValue = sub1.get(folderKey, "default");
+				if (folderValue.equals("default"))
+					break;
+				excludedFolders.add(folderValue);
+				i++;
+			}
+		}
+
 		logger.info("Start parsing " + project.getID() + " for annotations");
-		
-		if(project == null) {
+
+		if (project == null) {
 			monitor.done();
 			return Status.CANCEL_STATUS;
 		}
-		
+
 		try {
-			if(jobType == JobType.FULL) {
+			if (jobType == JobType.FULL) {
 				handleResource(project.getIProject(), monitor);
 				project.addFeatures(projectFeatures.values());
 			} else {
-				if(fileToParse == null) {
+				if (fileToParse == null) {
 					monitor.done();
 					return Status.CANCEL_STATUS;
 				}
-				if(equalsMappingFile(fileToParse) && !project.getOutputFolders().stream().map(IPath::toString).anyMatch(fileToParse.getFullPath().toString()::contains))
+				if (equalsMappingFile(fileToParse) && !project.getOutputFolders().stream().map(IPath::toString)
+						.anyMatch(fileToParse.getFullPath().toString()::contains))
 					handleMappingFile(fileToParse, monitor);
-				else if(fileToParse.getName().contains(".cfr"))
+				else if (fileToParse.getName().contains(".cfr"))
 					handleFeatureModel(fileToParse, monitor);
-				else 
+				else
 					handleFile(fileToParse, monitor);
 			}
-			
-			if(parsingExceptions.size() > 0) {
-				
+
+			if (parsingExceptions.size() > 0) {
+
 				StringBuilder errorMessage = new StringBuilder();
-				
+
 				parsingExceptions.forEach(wrongSyntax -> {
-					if(wrongSyntax.getRight() == null)
+					if (wrongSyntax.getRight() == null)
 						errorMessage.append(wrongSyntax.getLeft() + "  ---> " + wrongSyntax.getCenter());
 					else
-						errorMessage.append(wrongSyntax.getLeft() + "  ---> " + wrongSyntax.getCenter() + " at line number: " + wrongSyntax.getRight());
+						errorMessage.append(wrongSyntax.getLeft() + "  ---> " + wrongSyntax.getCenter()
+								+ " at line number: " + wrongSyntax.getRight());
 					errorMessage.append("\n");
 				});
-				
+
 				Display.getDefault().asyncExec(() -> {
-					errorDialog = new ErrorDialog(shell, FeaturedashboardConstants.SYNTAXERROR_DIALOG_TITLE, FeaturedashboardConstants.SYNTAXERROR_DIALOG_MESSAGE, errorMessage.toString());
+					errorDialog = new ErrorDialog(shell, FeaturedashboardConstants.SYNTAXERROR_DIALOG_TITLE,
+							FeaturedashboardConstants.SYNTAXERROR_DIALOG_MESSAGE, errorMessage.toString());
 					errorDialog.create();
 					errorDialog.open();
 				});
 			}
-			
+
 			monitor.done();
-			
-			if(monitor.isCanceled()) {
+
+			if (monitor.isCanceled()) {
 				System.out.println("monitor was canceled");
-				if(jobType == JobType.FULL) {
+				if (jobType == JobType.FULL) {
 					ProjectStore.removeProject(project);
 				}
-					
+
 				return Status.CANCEL_STATUS;
-			}
-			else
+			} else
 				return Status.OK_STATUS;
-			
-		} catch(RuntimeException e) {
-			// So that we can remove the project from the ProjectStore, otherwise the user has to close the IDE to re-parse the project
+
+		} catch (RuntimeException e) {
+			// So that we can remove the project from the ProjectStore, otherwise the user
+			// has to close the IDE to re-parse the project
 			logger.warn("Runtime exception occured: " + e.getMessage());
-			
+
 			e.printStackTrace();
-			
+
 			Display.getDefault().asyncExec(() -> {
 				MessageDialog.openError(shell, "Error!", "Runtime error while parsing project: " + e.getMessage());
 			});
-			
+
 			monitor.done();
-			
-			if(jobType == JobType.FULL)
+
+			if (jobType == JobType.FULL)
 				ProjectStore.removeProject(project);
-			
+
 			return Status.CANCEL_STATUS;
 		}
-		 
+
 	}
-	
+
 	private void handleResource(IContainer container, IProgressMonitor monitor) {
-		if(monitor.isCanceled())
+		if (monitor.isCanceled())
 			return;
-		
+
 		try {
-			
-			Arrays.stream(container.members()).filter(this::equalsMappingFile).forEach(resource -> handleMappingFile((IFile) resource, monitor));
-			
+			Arrays.stream(container.members()).filter(this::equalsMappingFile)
+					.forEach(resource -> handleMappingFile((IFile) resource, monitor));
+
 			Arrays.stream(container.members()).forEach(member -> {
-				if(member instanceof IContainer) {
-					if(!project.getOutputFolders().stream().map(IPath::toString).anyMatch(member.getFullPath().toString()::contains))
+				if (member instanceof IContainer) {
+					if (!project.getOutputFolders().stream().map(IPath::toString)
+							.anyMatch(member.getFullPath().toString()::contains)
+							&& !excludedFolders.stream().anyMatch(member.getFullPath().toString()::contains))
 						handleResource((IContainer) member, monitor);
-				} else if(member instanceof IFile) {
+				} else if (member instanceof IFile) {
 					IFile file = (IFile) member;
-					if(file.getName().contains(".cfr"))
+					if (file.getName().contains(".cfr"))
 						handleFeatureModel(file, monitor);
 					handleFile(file, monitor);
 				}
 			});
-		} catch(CoreException e) {
-			parsingExceptions.add(new Triple<String, String, Integer>(container.getFullPath().toString(), e.getMessage(), null));
+		} catch (CoreException e) {
+			parsingExceptions
+					.add(new Triple<String, String, Integer>(container.getFullPath().toString(), e.getMessage(), null));
 			monitor.setCanceled(true);
 		}
 	}
-	
+
 	private void handleFeatureModel(IFile file, IProgressMonitor monitor) {
 		ClaferFileParser parser = new ClaferFileParser(file);
 		FeatureModelHierarchy featureModelHierarchy = parser.readParse();
-		
+
 		project.setRootFeatures(featureModelHierarchy.getRootFeatures());
 		project.setFeatureModel(featureModelHierarchy.getAllFeatures());
-		
-		for(Feature feature : featureModelHierarchy.getAllFeatures()) {
+
+		for (Feature feature : featureModelHierarchy.getAllFeatures()) {
 			FeatureContainer featureContainer = null;
-			if(jobType == JobType.FULL) {
+			if (jobType == JobType.FULL) {
 				featureContainer = projectFeatures.get(feature);
 			} else {
 				featureContainer = project.getFeatureContainer(feature);
 			}
-			
-			if(featureContainer == null) {
+
+			if (featureContainer == null) {
 				featureContainer = new FeatureContainer(feature);
-				if(jobType == JobType.FULL)
+				if (jobType == JobType.FULL)
 					projectFeatures.put(feature, featureContainer);
 				else
 					project.addFeatureContainer(featureContainer);
 			} else {
 				featureContainer.setFeature(feature);
 			}
-		}	
+		}
 	}
-	
+
 	private void handleFile(IFile resource, IProgressMonitor monitor) {
-		if(monitor.isCanceled())
+		if (monitor.isCanceled())
 			return;
-		
+
 		List<FeatureContainer> containersImplementedInFile = null;
-		
-		if(jobType == JobType.SINGLE)
-			containersImplementedInFile = project.getFeatureContainers().stream().filter(c -> c.isAnnotatedIn(resource)).collect(Collectors.toList());
-					
+
+		if (jobType == JobType.SINGLE)
+			containersImplementedInFile = project.getFeatureContainers().stream().filter(c -> c.isAnnotatedIn(resource))
+					.collect(Collectors.toList());
+
 		Map<Feature, List<BlockLine>> featureToLines = new HashMap<>();
-		parser.readParseAnnotations(new ArrayList<IFile>(Arrays.asList(resource))).stream().forEach(location -> featureToLines.put(location.getFeature(), location.getBlocklines()));
-		
+		parser.readParseAnnotations(new ArrayList<IFile>(Arrays.asList(resource))).stream()
+				.forEach(location -> featureToLines.put(location.getFeature(), location.getBlocklines()));
+
 		int uniqueFeatures = featureToLines.keySet().size();
-		
+
 		featureToLines.entrySet().stream().forEach(entry -> {
 			FeatureContainer featureContainer = getFeatureContainer(entry.getKey());
-			featureContainer.addInFileAnnotations(resource, entry.getValue(), uniqueFeatures-1);
+			featureContainer.addInFileAnnotations(resource, entry.getValue(), uniqueFeatures - 1);
 		});
-		
-		if(jobType == JobType.SINGLE) {
-			//ObjectToFileHandler handler = ObjectToFileHandler.getInstance();
-			
-			for(FeatureContainer fc : containersImplementedInFile) {
-				if(!featureToLines.containsKey(fc.getFeature())) {
+
+		if (jobType == JobType.SINGLE) {
+			// ObjectToFileHandler handler = ObjectToFileHandler.getInstance();
+
+			for (FeatureContainer fc : containersImplementedInFile) {
+				if (!featureToLines.containsKey(fc.getFeature())) {
 					fc.removeFile(resource);
-					if(fc.getScatteringDegree() == 0) {
+					if (fc.getScatteringDegree() == 0) {
 						project.removeFeature(fc);
-						//handler.removeFile(fc);
+						// handler.removeFile(fc);
 						continue;
 					}
 				}
-				//handler.writeObjectToFile(fc);
+				// handler.writeObjectToFile(fc);
 			}
 		}
 	}
-	
+
 	private void handleMappingFile(IFile mappingFile, IProgressMonitor monitor) {
-		if(monitor.isCanceled())
+		if (monitor.isCanceled())
 			return;
-		
+
 		try {
 			List<FeatureContainer> containersMappedTo = null;
-			
-			if(jobType == JobType.SINGLE)
-				containersMappedTo = project.getFeatureContainers().stream().filter(c -> c.isMappedIn(mappingFile)).collect(Collectors.toList());
-			
-			Map<Feature, List<IResource>> mapping = ParseMappingFile.readMappingFile(mappingFile, project.getIProject());
-			
-			mapping.keySet().forEach(feature -> {				
+
+			if (jobType == JobType.SINGLE)
+				containersMappedTo = project.getFeatureContainers().stream().filter(c -> c.isMappedIn(mappingFile))
+						.collect(Collectors.toList());
+
+			Map<Feature, List<IResource>> mapping = ParseMappingFile.readMappingFile(mappingFile,
+					project.getIProject());
+
+			mapping.keySet().forEach(feature -> {
 				List<Tuple<IResource, Integer>> folderResources = new ArrayList<>();
-				
-				if(mappingFile.getFileExtension().equals(FeaturedashboardConstants.VPFOLDER_FILE))
+
+				if (mappingFile.getFileExtension().equals(FeaturedashboardConstants.VPFOLDER_FILE))
 					folderResources.add(new Tuple<IResource, Integer>(mappingFile.getParent(), 0));
-				
+
 				List<IResource> resources = mapping.get(feature);
-				
-				for(IResource resource : resources)
+
+				for (IResource resource : resources)
 					mapResourceToFeature(feature, resource, folderResources, monitor);
-				
-				FeatureContainer featureContainer = getFeatureContainer(feature);				
+
+				FeatureContainer featureContainer = getFeatureContainer(feature);
 				featureContainer.addMappingResource(mappingFile, folderResources);
 			});
-			
-			if(jobType == JobType.SINGLE) {
-				containersMappedTo.stream()
-					.filter(c -> !mapping.containsKey(c.getFeature()))
-						.forEach(c -> {
-							c.removeFile(mappingFile);
-							if(c.getScatteringDegree() == 0)
-								project.removeFeature(c);
-						});
+
+			if (jobType == JobType.SINGLE) {
+				containersMappedTo.stream().filter(c -> !mapping.containsKey(c.getFeature())).forEach(c -> {
+					c.removeFile(mappingFile);
+					if (c.getScatteringDegree() == 0)
+						project.removeFeature(c);
+				});
 			}
-			
-		} catch(SyntaxException e) {
-			parsingExceptions.add(new Triple<String, String, Integer>(mappingFile.getFullPath().toString(), e.getMessage(), e.getLineNumber()));
+
+		} catch (SyntaxException e) {
+			parsingExceptions.add(new Triple<String, String, Integer>(mappingFile.getFullPath().toString(),
+					e.getMessage(), e.getLineNumber()));
 		}
 	}
-	
-	private void mapResourceToFeature(Feature feature, IResource resource, List<Tuple<IResource, Integer>> folderResources, IProgressMonitor monitor) {
-		if(monitor.isCanceled())
+
+	private void mapResourceToFeature(Feature feature, IResource resource,
+			List<Tuple<IResource, Integer>> folderResources, IProgressMonitor monitor) {
+		if (monitor.isCanceled())
 			return;
-		
+
 		try {
-			if(resource instanceof IContainer) {
+			if (resource instanceof IContainer) {
 				IFolder folder = (IFolder) resource;
 				IResource[] members = folder.members();
-				
+
 				folderResources.add(new Tuple<IResource, Integer>(folder, 0));
-				Arrays.stream(members).forEach(member -> mapResourceToFeature(feature, member, folderResources, monitor));
+				Arrays.stream(members)
+						.forEach(member -> mapResourceToFeature(feature, member, folderResources, monitor));
 			} else {
 				IFile leafFile = (IFile) resource;
 				int lineCount = countLines(leafFile);
-				
+
 				folderResources.add(new Tuple<IResource, Integer>(leafFile, lineCount));
 			}
 		} catch (CoreException | IOException e) {
-			parsingExceptions.add(new Triple<String, String, Integer>(resource.getFullPath().toString(), "Error trying to map resource to feature. " + e.getMessage(), null));
+			parsingExceptions.add(new Triple<String, String, Integer>(resource.getFullPath().toString(),
+					"Error trying to map resource to feature. " + e.getMessage(), null));
 			monitor.setCanceled(true);
 		}
 	}
-	
+
 	private FeatureContainer getFeatureContainer(Feature feature) {
 		FeatureContainer featureContainer = null;
-		if(jobType == JobType.FULL)
+		if (jobType == JobType.FULL)
 			featureContainer = projectFeatures.get(feature);
 		else
 			featureContainer = project.getFeatureContainer(feature);
-	
-		if(featureContainer == null) {
+
+		if (featureContainer == null) {
 			featureContainer = new FeatureContainer(feature);
-			if(jobType == JobType.FULL)
+			if (jobType == JobType.FULL)
 				projectFeatures.put(feature, featureContainer);
 			else
 				project.addFeatureContainer(featureContainer);
 		}
 		return featureContainer;
 	}
-	
+
 	private boolean equalsMappingFile(IResource resource) {
-		if(!(resource instanceof IFile))
+		if (!(resource instanceof IFile))
 			return false;
-		
-		if(!resource.getName().contains("."))
+
+		if (!resource.getName().contains("."))
 			return false;
-		
-		return resource.getFileExtension().equals(FeaturedashboardConstants.FEATUREFILE_FILE) || 
-				resource.getFileExtension().equals(FeaturedashboardConstants.FEATUREFOLDER_FILE) || 
-				resource.getFileExtension().equals(FeaturedashboardConstants.VPFILE_FILE) ||
-				resource.getFileExtension().equals(FeaturedashboardConstants.VPFOLDER_FILE);
+
+		return resource.getFileExtension().equals(FeaturedashboardConstants.FEATUREFILE_FILE)
+				|| resource.getFileExtension().equals(FeaturedashboardConstants.FEATUREFOLDER_FILE)
+				|| resource.getFileExtension().equals(FeaturedashboardConstants.VPFILE_FILE)
+				|| resource.getFileExtension().equals(FeaturedashboardConstants.VPFOLDER_FILE);
 	}
-	
+
 	private int countLines(IFile file) throws IOException, CoreException {
 		int count = 0;
-		try (InputStream is = new BufferedInputStream(file.getContents())){
-	        byte[] c = new byte[1024];
-	        int readChars = 0;
-	        boolean endsWithoutNewLine = false;
-	        while ((readChars = is.read(c)) != -1) {
-	            for (int i = 0; i < readChars; ++i) {
-	                if (c[i] == '\n')
-	                    ++count;
-	            }
-	            endsWithoutNewLine = (c[readChars - 1] != '\n');
-	        }
-	        if(endsWithoutNewLine) {
-	            ++count;
-	        } 
-	    }
-	    return count;
+		try (InputStream is = new BufferedInputStream(file.getContents())) {
+			byte[] c = new byte[1024];
+			int readChars = 0;
+			boolean endsWithoutNewLine = false;
+			while ((readChars = is.read(c)) != -1) {
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n')
+						++count;
+				}
+				endsWithoutNewLine = (c[readChars - 1] != '\n');
+			}
+			if (endsWithoutNewLine) {
+				++count;
+			}
+		}
+		return count;
 	}
 }
